1d40ea40a97eb4d812e7c6b6fe90d6fbdf4faf3a
Richard Wells <rwells1703@gmail.com>
2022-04-12 14:32:02 +0100

    Updated SPI device address of fingeprint reader.

--- fingerprint/example/bep/rpispi.py
+++ fingerprint/example/bep/rpispi.py
@@ -30,7 +30,7 @@ class Rpispi(object):
 
     def __init__(self):
         self.spi = spidev.SpiDev()
-        self.port = "SPI1"
+        self.port = "SPI0"
         self.timeout = 0
         GPIO.setmode(GPIO.BCM)
 
@@ -38,7 +38,7 @@ class Rpispi(object):
         """Connect to Rpi SPI interface"""
         # Speed must be exact rates so hardcoded here for now
         speed = 976000
-        self.spi.open(1, 2)
+        self.spi.open(0, 0)
         self.spi.max_speed_hz = speed
         self.timeout = timeout
         GPIO.setup(HOST_IRQ_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
