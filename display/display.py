84f030fbff097ea2ddb79db910a3f0e20e8b88d5
Richard Wells <rwells1703@gmail.com>
2022-05-01 16:44:33 +0100

    Menu now controlled via external buttons, not console.

--- display/display.py
+++ display/display.py
@@ -1,3 +1,6 @@
+import time
+import RPi.GPIO as GPIO
+
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
@@ -9,8 +12,27 @@ class Display():
         self.initialise()
 
     def initialise(self):
+        # Connect to the OLED display over i2c
         self.serial = i2c(port=1, address=0x3C)
+        
+        # Use the sh1106 driver
         self.device = sh1106(self.serial)
+
+        # Define the input buttons and their GPIO pins
+        self.buttons = {
+            "top": {"gpio": 16, "pressed": False},
+            "bottom": {"gpio": 12, "pressed": False}
+            }
+
+        # Prevent warnings if the GPIO pin roles have already been set previously
+        GPIO.setwarnings(False)
+
+        # Set the GPIO pin numbering mode to BCM
+        GPIO.setmode(GPIO.BCM)
+
+        # Set the GPIO pins connecting the buttons to pull up
+        for button in self.buttons.values():
+            GPIO.setup(button["gpio"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def centred_text_coords(self, draw, text):
         w, h = draw.textsize(text)
@@ -49,13 +71,22 @@ class Display():
                     self.draw_centred_text(draw, entry, x_pos=border, y_pos=(i*8)+14)
                     i += 1
 
-            choice = input()
+            self.poll_buttons()
 
-            if choice == "y":
-                back = list(menu.values())[selected]()
-            else:
+            if self.buttons["top"]["pressed"]:
                 selected = (selected+1)%len(menu)
+            elif self.buttons["bottom"]["pressed"]:
+                back = list(menu.values())[selected]()
+
+            time.sleep(0.01)
     
+    def poll_buttons(self):
+        for button in self.buttons.values():
+            if not GPIO.input(button["gpio"]):
+                button["pressed"] = True
+            else:
+                button["pressed"] = False
+
     def draw_message(self, message):
         with canvas(self.device) as draw:
             self.draw_centred_text(draw, message)
