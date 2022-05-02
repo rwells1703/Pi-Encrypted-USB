fdcce7966d00315cd6dafd5a92a85730f3492079
Richard Wells <rwells1703@gmail.com>
2022-05-02 19:09:06 +0100

    Standardised script file paths in secure wrapper code.

--- fingerprint/secure_com_wrapper.py
+++ fingerprint/secure_com_wrapper.py
@@ -13,14 +13,6 @@ class ComSecure(fingerprint.bep.communication.Com):
     def __init__(self, cmd, argument=None, mtu=256):
         super().__init__(cmd, argument, mtu)
 
-    def _tx_application(self, serial):
-        print("transmitting")
-        return super()._tx_application(serial)
-
-    def _rx_application(self, serial):
-        print("receiving")
-        return super()._rx_application(serial)
-
     def _tx_transport(self, serial, seq_nr, seq_len, app_data):
         app_data = self.send_app_data(app_data)
 
@@ -33,16 +25,16 @@ class ComSecure(fingerprint.bep.communication.Com):
 
         # Write the app data to a temporary file so it can be read by the encryption script
         # (It cannot be input as a command line argument as it may contain null bytes)
-        f = open("storage/ramdisk/app_data.dat", "wb")
+        f = open("./storage/ramdisk/app_data.dat", "wb")
         combined_data = app_data + (";" + app_data_sig).encode("utf-8")
         f.write(combined_data)
         f.close()
 
         # Concatenate the data with its signature, then asymmetrically encrypt this data using the raspberry pi's public key
-        app_data_encrypted = encryption.Encryption.asymm_encrypt_data(RASPBERRY_KEY_ADDR, "storage/ramdisk/app_data.dat")
+        app_data_encrypted = encryption.Encryption.asymm_encrypt_data(RASPBERRY_KEY_ADDR, "./storage/ramdisk/app_data.dat")
 
         # Remove the app data file
-        os.remove("storage/ramdisk/app_data.dat")
+        os.remove("./storage/ramdisk/app_data.dat")
 
         # Delete the plaintext version of the data
         app_data = None
@@ -52,15 +44,15 @@ class ComSecure(fingerprint.bep.communication.Com):
         # --->
 
         # Again write the (now encrypted) app data to a temporary file for reading
-        f = open("storage/ramdisk/app_data_encrypted.dat", "wb")
+        f = open("./storage/ramdisk/app_data_encrypted.dat", "wb")
         f.write(app_data_encrypted)
         f.close()
 
         # Decrypt the command and signature using the raspberry pi's private key
-        app_data_decrypted = encryption.Encryption.asymm_decrypt_data(RASPBERRY_KEY_ADDR, "storage/ramdisk/app_data_encrypted.dat")
+        app_data_decrypted = encryption.Encryption.asymm_decrypt_data(RASPBERRY_KEY_ADDR, "./storage/ramdisk/app_data_encrypted.dat")
 
         # Remove the encrypted app data file
-        os.remove("storage/ramdisk/app_data_encrypted.dat")
+        os.remove("./storage/ramdisk/app_data_encrypted.dat")
 
         # Extract both the decrypted command, and the digital signature from the data blob
         app_data = app_data_decrypted.split(b';')[0]
